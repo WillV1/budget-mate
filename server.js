cfd0bea69c91d4405cc5b951cb36c1746aab880d
WillV1 <wmvanhook@gmail.com>
2020-08-30 22:25:49 -0400

    Initial server commit

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 
+app.get('/', (req, res) => res.send('API Running'));
+
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
